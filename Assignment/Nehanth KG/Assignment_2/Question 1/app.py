e8b6034bb9c7f1796b188e9cfe85eeb5a7eb612c
NehanthKG <nehanthkg@gmail.com>
2022-10-10 13:46:52 +0530

    Added assignment 2

---
+++ Assignment/Nehanth KG/Assignment_2/Question 1/app.py
@@ -0,0 +1,26 @@
+from unicodedata import name
+from flask import Flask, render_template, request, redirect, url_for, session
+import re
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('form.html')
+
+
+@app.route('/next', methods=['POST'])
+def register():
+    print("hello")
+    if request.method == 'POST':
+        username = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        # msg = "Welcome " + username + " " + email + " " + phone
+
+    return render_template('next.html', name=username, email=email, phone=phone)
+
+
+if (__name__ == '__main__'):
+    app.run()
