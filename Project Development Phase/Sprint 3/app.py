7eb84c49764402109d9beb3e3f546cd0546fe54f
Harishankar <harishankar0301@gmail.com>
2022-11-18 07:55:02 +0530

    Sprint 3

---
+++ Project Development Phase/Sprint 3/app.py
@@ -0,0 +1,448 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators, SelectField, IntegerField
+import ibm_db
+from functools import wraps
+
+app = Flask(__name__)
+app.secret_key = 'meow_cat'
+#IBM DB2 Connection
+try:
+    conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=b0aebb68-94fa-46ec-a1fc-1c999edb6187.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud;PORT=31249;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=cqg39702;PWD=hIRRyoYSNHJxjqQq", "", "")
+except:
+    print("Unable to connect: ", ibm_db.conn_error())
+#Home Page
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+#Register Form Class
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=1, max=25)])
+    email = StringField('Email', [validators.length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+#user register
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = str(form.password.data)
+
+        sql = "SELECT * FROM users WHERE email=?"
+        prep_stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(prep_stmt, 1, email)
+        ibm_db.execute(prep_stmt)
+        account = ibm_db.fetch_assoc(prep_stmt)
+        print(account)
+        if account:
+            error = "Account already exists! Log in to continue !"
+        else:
+            insert_sql = "INSERT INTO users (email,username,password,name) values(?,?,?)"
+            prep_stmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(prep_stmt, 1, email)
+            ibm_db.bind_param(prep_stmt, 2, username)
+            ibm_db.bind_param(prep_stmt, 3, password)
+            ibm_db.bind_param(prep_stmt, 4, name)
+            ibm_db.execute(prep_stmt)
+            flash(" Registration successful. Log in to continue !")
+               
+        #when registration is successful redirect to home
+        return redirect(url_for('login'))
+    return render_template('register.html', form = form)
+
+#User login
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        error = None
+        account = None
+        #Get form fields
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
+
+        sql = "SELECT * FROM users WHERE username=? AND password=?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt, 1, username)
+        ibm_db.bind_param(stmt, 2, password)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        print(account)
+    if account:
+        session['logged_in'] = True
+        session['username'] = username
+        flash("Logged in successfully","success")
+        return redirect(url_for('dashboard'))
+    else: 
+        error = "Incorrect username / password"
+        return render_template('login.html', error=error)
+
+
+#Is Logged In
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/dashboard')
+@is_logged_in 
+def dashboard():
+    sql = "SELECT * FROM stocks"
+    stmt = ibm_db.exec_immediate(conn, sql)
+    dictionary = ibm_db.fetch_assoc(stmt)
+    stocks = []
+    print(dictionary)
+    headings = [*dictionary]
+    while dictionary != False:
+        stocks.append(dictionary)
+        dictionary = ibm_db.fetch_assoc(stmt)
+    return render_template('dashboard.html',headings=headings, data=stocks)
+
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash("Logged out successfully", "success")
+    return redirect(url_for('login'))
+
+@app.route('/inventoryUpdate', methods=['POST'])
+@is_logged_in
+def inventoryUpdate():
+    if request.method == "POST":
+        try:
+            item = request.form['item']
+            print("hello")
+            field = request.form['input-field']
+            value = request.form['input-value']
+            print(item, field, value)
+            insert_sql = 'UPDATE stocks SET ' + field + "= ?" + " WHERE NAME=?"
+            print(insert_sql)
+            pstmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(pstmt, 1, value)
+            ibm_db.bind_param(pstmt, 2, item)
+            ibm_db.execute(pstmt)
+            if field == 'PRICE_PER_QUANTITY' or field == 'QUANTITY':
+                insert_sql = 'SELECT * FROM stocks WHERE NAME= ?'
+                pstmt = ibm_db.prepare(conn, insert_sql)
+                ibm_db.bind_param(pstmt, 1, item)
+                ibm_db.execute(pstmt)
+                dictonary = ibm_db.fetch_assoc(pstmt)
+                print(dictonary)
+                total = dictonary['QUANTITY'] * dictonary['PRICE_PER_QUANTITY']
+                insert_sql = 'UPDATE stocks SET TOTAL_PRICE=? WHERE NAME=?'
+                pstmt = ibm_db.prepare(conn, insert_sql)
+                ibm_db.bind_param(pstmt, 1, total)
+                ibm_db.bind_param(pstmt, 2, item)
+                ibm_db.execute(pstmt)
+        except Exception as e:
+            msg = e
+
+        finally:
+            
+            return redirect(url_for('dashboard'))
+
+@app.route('/addstocks', methods=['POST'])
+@is_logged_in
+def addStocks():
+    if request.method == "POST":
+        print(request.form['item'])
+        try:
+            item = request.form['item']
+            quantity = request.form['quantity']
+            price = request.form['price']
+            total = int(price) * int(quantity)
+            insert_sql = 'INSERT INTO stocks (NAME,QUANTITY,PRICE_PER_QUANTITY,TOTAL_PRICE) VALUES (?,?,?,?)'
+            pstmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(pstmt, 1, item)
+            ibm_db.bind_param(pstmt, 2, quantity)
+            ibm_db.bind_param(pstmt, 3, price)
+            ibm_db.bind_param(pstmt, 4, total)
+            ibm_db.execute(pstmt)
+
+        except Exception as e:
+            msg = e
+
+        finally:
+
+            return redirect(url_for('dashboard'))
+
+@app.route('/deletestocks', methods=['POST'])
+@is_logged_in
+def deleteStocks():
+    if request.method == "POST":
+        print(request.form['item'])
+        try:
+            item = request.form['item']
+            insert_sql = 'DELETE FROM stocks WHERE NAME=?'
+            pstmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(pstmt, 1, item)
+            ibm_db.execute(pstmt)
+        except Exception as e:
+            msg = e
+
+        finally:
+            return redirect(url_for('dashboard'))
+
+
+@app.route('/update-user', methods=['POST', 'GET'])
+@is_logged_in
+def updateUser():
+    if request.method == "POST":
+        try:
+            email = session['username']
+            field = request.form['input-field']
+            value = request.form['input-value']
+            insert_sql = 'UPDATE users SET ' + field + '= ? WHERE username=?'
+            pstmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(pstmt, 1, value)
+            ibm_db.bind_param(pstmt, 2, email)
+            print(pstmt)
+            ibm_db.execute(pstmt)
+        except Exception as e:
+            print(e)
+            msg = e
+
+        finally:
+            if field == 'USERNAME':
+                session['username'] = value
+            return redirect(url_for('profile'))
+
+
+@app.route('/update-password', methods=['POST', 'GET'])
+@is_logged_in
+def updatePassword():
+    if request.method == "POST":
+        try:
+            email = session['username']
+            password = request.form['prev-password']
+            curPassword = request.form['cur-password']
+            confirmPassword = request.form['confirm-password']
+            insert_sql = 'SELECT * FROM  users WHERE username=? AND PASSWORD=?'
+            pstmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(pstmt, 1, email)
+            ibm_db.bind_param(pstmt, 2, password)
+            ibm_db.execute(pstmt)
+            dictionary = ibm_db.fetch_assoc(pstmt)
+            print(dictionary)
+            if curPassword == confirmPassword:
+                insert_sql = 'UPDATE users SET PASSWORD=? WHERE username=?'
+                pstmt = ibm_db.prepare(conn, insert_sql)
+                ibm_db.bind_param(pstmt, 1, confirmPassword)
+                ibm_db.bind_param(pstmt, 2, email)
+                ibm_db.execute(pstmt)
+        except Exception as e:
+            msg = e
+        finally:
+           
+            return render_template('result.html')
+
+
+@app.route('/orders', methods=['POST', 'GET'])
+@is_logged_in
+def orders():
+    query = "SELECT * FROM orders"
+    stmt = ibm_db.exec_immediate(conn, query)
+    dictionary = ibm_db.fetch_assoc(stmt)
+    orders = []
+    headings = [*dictionary]
+    while dictionary != False:
+        orders.append(dictionary)
+        dictionary = ibm_db.fetch_assoc(stmt)
+    return render_template("orders.html", headings=headings, data=orders)
+
+
+@app.route('/createOrder', methods=['POST'])
+@is_logged_in
+def createOrder():
+    if request.method == "POST":
+        try:
+            stock_id = request.form['stock_id']
+            query = 'SELECT PRICE_PER_QUANTITY FROM stocks WHERE ID= ?'
+            stmt = ibm_db.prepare(conn, query)
+            ibm_db.bind_param(stmt, 1, stock_id)
+            ibm_db.execute(stmt)
+            dictionary = ibm_db.fetch_assoc(stmt)
+            if dictionary:
+                quantity = request.form['quantity']
+                date = str(datetime.now().year) + "-" + str(
+                    datetime.now().month) + "-" + str(datetime.now().day)
+                delivery = datetime.now() + timedelta(days=7)
+                delivery_date = str(delivery.year) + "-" + str(
+                    delivery.month) + "-" + str(delivery.day)
+                price = float(quantity) * \
+                    float(dictionary['PRICE_PER_QUANTITY'])
+                query = 'INSERT INTO orders (STOCKS_ID,QUANTITY,DATE,DELIVERY_DATE,PRICE) VALUES (?,?,?,?,?)'
+                pstmt = ibm_db.prepare(conn, query)
+                ibm_db.bind_param(pstmt, 1, stock_id)
+                ibm_db.bind_param(pstmt, 2, quantity)
+                ibm_db.bind_param(pstmt, 3, date)
+                ibm_db.bind_param(pstmt, 4, delivery_date)
+                ibm_db.bind_param(pstmt, 5, price)
+                ibm_db.execute(pstmt)
+        except Exception as e:
+            print(e)
+
+        finally:
+            return redirect(url_for('orders'))
+
+
+@app.route('/updateOrder', methods=['POST'])
+@is_logged_in
+def updateOrder():
+    if request.method == "POST":
+        try:
+            item = request.form['item']
+            field = request.form['input-field']
+            value = request.form['input-value']
+            query = 'UPDATE orders SET ' + field + "= ?" + " WHERE ID=?"
+            pstmt = ibm_db.prepare(conn, query)
+            ibm_db.bind_param(pstmt, 1, value)
+            ibm_db.bind_param(pstmt, 2, item)
+            ibm_db.execute(pstmt)
+        except Exception as e:
+            print(e)
+
+        finally:
+            return redirect(url_for('orders'))
+
+
+@app.route('/cancelOrder', methods=['POST'])
+@is_logged_in
+def cancelOrder():
+    if request.method == "POST":
+        try:
+            order_id = request.form['order_id']
+            query = 'DELETE FROM orders WHERE ID=?'
+            pstmt = ibm_db.prepare(conn, query)
+            ibm_db.bind_param(pstmt, 1, order_id)
+            ibm_db.execute(pstmt)
+        except Exception as e:
+            print(e)
+
+        finally:
+            return redirect(url_for('orders'))
+
+
+@app.route('/suppliers', methods=['POST', 'GET'])
+@is_logged_in
+def suppliers():
+    sql = "SELECT * FROM suppliers"
+    stmt = ibm_db.exec_immediate(conn, sql)
+    dictionary = ibm_db.fetch_assoc(stmt)
+    suppliers = []
+    orders_assigned = []
+    headings = [*dictionary]
+    while dictionary != False:
+        suppliers.append(dictionary)
+        orders_assigned.append(dictionary['ORDER_ID'])
+        dictionary = ibm_db.fetch_assoc(stmt)
+
+# get order ids from orders table and identify unassigned order ids
+    sql = "SELECT ID FROM orders"
+    stmt = ibm_db.exec_immediate(conn, sql)
+    dictionary = ibm_db.fetch_assoc(stmt)
+    order_ids = []
+    while dictionary != False:
+        order_ids.append(dictionary['ID'])
+        dictionary = ibm_db.fetch_assoc(stmt)
+
+    unassigned_order_ids = set(order_ids) - set(orders_assigned)
+    return render_template("suppliers.html", headings=headings, data=suppliers, order_ids=unassigned_order_ids)
+
+
+@app.route('/updatesupplier', methods=['POST'])
+@is_logged_in
+def UpdateSupplier():
+    if request.method == "POST":
+        try:
+            item = request.form['name']
+            field = request.form['input-field']
+            value = request.form['input-value']
+            print(item, field, value)
+            insert_sql = 'UPDATE suppliers SET ' + field + "= ?" + " WHERE NAME=?"
+            print(insert_sql)
+            pstmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(pstmt, 1, value)
+            ibm_db.bind_param(pstmt, 2, item)
+            ibm_db.execute(pstmt)
+        except Exception as e:
+            msg = e
+
+        finally:
+            return redirect(url_for('suppliers'))
+
+
+@app.route('/addsupplier', methods=['POST'])
+@is_logged_in
+def addSupplier():
+    if request.method == "POST":
+        try:
+            name = request.form['name']
+            order_id = request.form.get('order-id-select')
+            print(order_id)
+            print("Hello world")
+            location = request.form['location']
+            insert_sql = 'INSERT INTO suppliers (NAME,ORDER_ID,LOCATION) VALUES (?,?,?)'
+            pstmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(pstmt, 1, name)
+            ibm_db.bind_param(pstmt, 2, order_id)
+            ibm_db.bind_param(pstmt, 3, location)
+            ibm_db.execute(pstmt)
+
+        except Exception as e:
+            msg = e
+
+        finally:
+            return redirect(url_for('suppliers'))
+
+
+@app.route('/deletesupplier', methods=['POST'])
+@is_logged_in
+def deleteSupplier():
+    if request.method == "POST":
+        try:
+            item = request.form['name']
+            insert_sql = 'DELETE FROM suppliers WHERE NAME=?'
+            pstmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(pstmt, 1, item)
+            ibm_db.execute(pstmt)
+        except Exception as e:
+            msg = e
+
+        finally:
+            return redirect(url_for('suppliers'))
+
+
+@app.route('/profile', methods=['POST', 'GET'])
+@is_logged_in
+def profile():
+    if request.method == "GET":
+     
+            email = session['username']
+            insert_sql = 'SELECT * FROM users WHERE username=?'
+            pstmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(pstmt, 1, email)
+            ibm_db.execute(pstmt)
+            dictionary = ibm_db.fetch_assoc(pstmt)
+            print(dictionary)
+            return render_template("profile.html", data=dictionary)
+
+if __name__ == '__main__':
+    
+    app.run(debug=True)
